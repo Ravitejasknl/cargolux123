e3bf94f2ec85139810771449d6fbe23d847eb8fc
Ravitejasknl <tejalovingsis@gmail.com>
2024-11-24 11:38:58 +0530

    checking git

---
+++ src/org/tcs/test/login/CourseDetails.java
@@ -0,0 +1,23 @@
+package org.tcs.test.login;
+
+public class CourseDetails {
+	
+	private void javaCourse() {
+	
+	System.out.println("java Course is Taknen");
+	}
+	
+	private void PythonCourse() {
+		
+	System.out.println("Python Course is not Taken");
+	
+	}
+   public static void main(String[] args) {
+	
+	   CourseDetails cd = new CourseDetails();
+			   
+			   cd.javaCourse();
+	   
+	          cd.PythonCourse();
+}
+}
