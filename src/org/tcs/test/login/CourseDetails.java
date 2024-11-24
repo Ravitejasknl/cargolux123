15ecdebf211a2855b7e2cc6dd1a3c75f018e5982
Ravitejasknl <tejalovingsis@gmail.com>
2024-11-24 13:54:53 +0530

    checking

--- src/org/tcs/test/login/CourseDetails.java
+++ src/org/tcs/test/login/CourseDetails.java
@@ -10,8 +10,17 @@ public class CourseDetails {
 	private void PythonCourse() {
 		
 	System.out.println("Python Course is not Taken");
+   
 	
 	}
+	private void gitcourse() {
+ System.out.println("git course");
+	}
+	private void giterror() {
+System.out.println("checking error");
+	}
+	
+	
    public static void main(String[] args) {
 	
 	   CourseDetails cd = new CourseDetails();
@@ -19,5 +28,7 @@ public class CourseDetails {
 			   cd.javaCourse();
 	   
 	          cd.PythonCourse();
+	          
+	          
 }
 }
